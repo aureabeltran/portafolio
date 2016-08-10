67a3776129fbf6156618c3138a86aff71395b990
aureabeltran <aureabeltranmanzo@gmail.com>
2016-08-10 15:31:53 -0400

    cambio funcion de los pop add

--- js/main.js
+++ js/main.js
@@ -3,36 +3,55 @@
 $("#pencil").on("click", function(){
 	var draw = $("#draw");
 	draw.toggleClass("_hide");
+	$('.contenedor').not(draw).addClass('_hide');
 })
 $("#joy").on("click", function(){
 	var game = $("#gamepad");
 	game.toggleClass("_hide");
+	$('.contenedor').not(game).addClass('_hide');
 })
 $("#smile").on("click", function(){
 	var face = $("#face");
 	face.toggleClass("_hide");
+	$('.contenedor').not(face).addClass('_hide');
 })
 $("#group").on("click", function(){
 	var people = $("#people");
 	people.toggleClass("_hide");
+	$('.contenedor').not(people).addClass('_hide');
 })
 $("#book").on("click", function(){
 	var note = $("#note");
 	note.toggleClass("_hide");
+	$('.contenedor').not(note).addClass('_hide');
 })
 $("#bateria").on("click", function(){
 	var batery = $("#batery");
 	batery.toggleClass("_hide");
+	$('.contenedor').not(batery).addClass('_hide');
 })
 $("#servicio").on("click", function(){
 	var tenedor = $("#tenedor");
 	tenedor.toggleClass("_hide");
+	$('.contenedor').not(tenedor).addClass('_hide');
 })
 $("#pc").on("click", function(){
 	var compu = $("#compu");
 	compu.toggleClass("_hide");
+	$('.contenedor').not(compu).addClass('_hide');
 })
 $("#bici").on("click", function(){
-	var compu = $("#cleta");
-	compu.toggleClass("_hide");
+	var cleta = $("#cleta");
+	cleta.toggleClass("_hide");
+	$('.contenedor').not(cleta).addClass('_hide');
+})
+
+ $(window).resize(function(){
+
+   if ($(window).width() < 420) {  
+
+          $('.contenedor').css({'width' : '200px' , 'left' : '0%'});
+
+   }     
+
 })
